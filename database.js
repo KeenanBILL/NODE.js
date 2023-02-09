72101329ab8fe067e9aa2ef03eb95c77c55fa089
KeenanBILL <t19008@tsiba.ac.za>
2023-02-09 16:48:26 +0200

    Commit

--- database.js
+++ database.js
@@ -1,9 +1,17 @@
 const { createPool } = require ('mysql')
 
+const pool = createPool ({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test',
+    connectionLimit: 10
+})
 
-pool.query('SELECT  * FROM signup', err, result, fields) => {
+
+pool.query('SELECT  * FROM signup', (err, result, fields) => {
     if(err) {
-        return console.log(result);
+        return console.error(err);
     }
-
-}
+    return console.log(result);
+});
